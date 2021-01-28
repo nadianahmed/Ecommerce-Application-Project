4ef7bd6324c5149fead98615dfb57f6f26a3ac60
Nadia Ahmed <nadiaahmed@Nadias-MacBook-Pro.local>
2021-01-27 22:35:44 -0500

    Initial commit

---
+++ src/Product.java
@@ -0,0 +1,23 @@
+public class Product {
+    private String name;
+    private String id;
+    private double price;
+
+    public Product(String name, String id, double price) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
