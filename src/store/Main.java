a072db423b5ad6921f7ddc3e9d2ee92be43ad496
Nadia Ahmed <76981861+nadianahmed@users.noreply.github.com>
2021-03-27 21:15:09 -0400

    Milestone 3 complete

--- src/store/Main.java
+++ src/store/Main.java
@@ -1,12 +1,12 @@
-package store;
-
 // Team Cup O' Java
 // Nadia Ahmed 101172713
 // Esraa Alaa Aldeen 101151604
+// Milestone 3
+package store;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Nothing to see here :)
     }
 
 }
--- src/store/ProductStock.java
+++ src/store/ProductStock.java
@@ -1,6 +1,7 @@
 // Team Cup O' Java
 // Nadia Ahmed 101172713
 // Esraa Alaa Aldeen 101151604
+// Milestone 3
 
 package store;
 
